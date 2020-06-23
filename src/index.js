9fb2b77e1643c0a36562efdc09c866dc211279ef
Benjamin Barbe <benjamin@pandasuite.com>
2020-06-23 16:28:26 +0200

    Iframe component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import PandaBridge from 'pandasuite-bridge';
 
 let properties = null;
-let markers = null;
 
 function myInit() {
-  // const imageUrl = PandaBridge.resolvePath('my_image.png');
-  // PandaBridge.send('imageChanged');
+  document.getElementById('gframe').src = properties.url;
 }
 
 PandaBridge.init(() => {
   PandaBridge.onLoad((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
 
     if (document.readyState === 'complete') {
       myInit();
@@ -22,22 +19,7 @@ PandaBridge.init(() => {
 
   PandaBridge.onUpdate((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
-  });
 
-  /* Markers */
-
-  PandaBridge.getSnapshotData(() => null);
-
-  PandaBridge.setSnapshotData((pandaData) => {
-    // pandaData.data.id
-  });
-
-  /* Actions */
-
-  PandaBridge.listen('changeColor', (args) => {
-  });
-
-  PandaBridge.synchronize('synchroImages', (percent) => {
+    myInit();
   });
 });
